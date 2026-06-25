484daa8619fa17d7a38fe9145c3d048a339a48b8
Jason Feimster <jasonfeimster@gmail.com>
2026-06-25 18:20:23 -0400

    Fix actions barrel duplicate exports (#60)

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -6,4 +6,25 @@ export * from "./sitemap-reader";
 export * from "./robots-check";
 
 export * from "../automation-readiness";
-export * from "../automation-opportunity-ranker";
+
+export {
+  DEFAULT_WEIGHTS,
+  FACTOR_KEYS,
+  FACTOR_META,
+  ValidationError as OpportunityRankerValidationError,
+  rankAutomationOpportunities,
+  validateOpportunityRankerRequest,
+} from "../automation-opportunity-ranker";
+
+export type {
+  FactorBreakdown,
+  OpportunityFactorKey,
+  OpportunityRankerRequest,
+  OpportunityRankerResponse,
+  OpportunityScores,
+  RankedOpportunity,
+  RecommendationCategory,
+  RecommendedAutomationMode,
+  ScoreValue as OpportunityScoreValue,
+  WorkflowOpportunityInput,
+} from "../automation-opportunity-ranker";
